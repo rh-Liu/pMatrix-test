176086251a643e0fa1d386fbd7ccc8f1a7f6ba57
algo21-221040051 <100055922+algo21-221040051@users.noreply.github.com>
2022-04-04 17:39:17 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='pMatrix',
+    version='1.0.0',
+    author='Ruihan Liu',
+    author_email='1398151992@qq.com',
+    url='https://github.com/algo21-221040051/pMatrix',
+    packages=[''],
+)
